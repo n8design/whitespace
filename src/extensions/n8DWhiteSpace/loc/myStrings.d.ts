6abe8c484e87ce286ae6a9a03fa901077b251b8d
Stefan Bauer <stefan.bauer@n8d.at>
2019-02-15 11:01:02 +0100

    blacklist css added

--- src/extensions/n8DWhiteSpace/loc/myStrings.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface IN8DWhiteSpaceApplicationCustomizerStrings {
-  Title: string;
-}
-
-declare module 'N8DWhiteSpaceApplicationCustomizerStrings' {
-  const strings: IN8DWhiteSpaceApplicationCustomizerStrings;
-  export = strings;
-}
