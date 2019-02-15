2d758618f7cab04a84d0a0e0e36b400d9d0a917b
Stefan Bauer <stefan.bauer@n8d.at>
2019-02-15 17:17:52 +0100

    Version

--- tools/post-version.js
+++ tools/post-version.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 
 const solution = './config/package-solution.json';
-const package = './config/package.json';
+const package = './package.json';
 
 const solutionFileContent = fs.readFileSync(solution, 'UTF-8');
 const packageFileContent = fs.readFileSync(package, 'UTF-8');
